9345d3aa45c075c5426ddba9dbef6a156492251f
Gillian-C-Gonzales <gillian.gonzales22@gmail.com>
2025-11-13 14:27:04 -0500

    Inital commit

---
+++ ofApp.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "ofMain.h"
+#include "../Sphere.h"
+
+class ofApp : public ofBaseApp {
+
+public:
+	void setup();
+	void update();
+	void draw();
+
+	int width;
+	int height;
+
+	Sphere firstSphere;
+	Sphere secondSphere;
+	Sphere thirdSphere;
+	Sphere forthSphere;
+};
